1c6e4f8908494d2b820fea8ab4891f080bea04f7
Poppe <djpo@users.noreply.github.com>
2022-08-11 01:44:41 +0200

    app displays rudimentary song cards

---
+++ SongCard.tsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+// interface Props {
+//   title: string;
+// }
+
+// const SongCard = ({ title }: Props) => {
+const SongCard: React.FC<{
+  title: string;
+}> = ({ title }) => {
+  return (
+    <View style={styles.card}>
+      <View style={styles.top}>
+        <Text>⭐️⭐️⭐️⭐️⭐️</Text>
+        <Text>img</Text>
+      </View>
+      <View style={styles.bottom}>
+        <Text>{title}</Text>
+        <Text>❤️</Text>
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  card: {
+    borderWidth: 2,
+    boderColor: "red",
+    padding: 10,
+  },
+  top: {
+    borderWidth: 2,
+    boderColor: "blue",
+  },
+  bottom: {
+    borderWidth: 2,
+    boderColor: "green",
+  },
+});
+
+export { SongCard };
