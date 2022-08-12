540bf529ac389a5ffec58d473206421ff5436c20
Poppe <djpo@users.noreply.github.com>
2022-08-12 12:58:42 +0200

    use react-testing/react-native

--- __tests__/SongCard-test.tsx
+++ __tests__/SongCard-test.tsx
@@ -1,9 +1,24 @@
 import "react-native";
 import React from "react";
-import renderer from "react-test-renderer";
+import { createStore } from "redux";
+import { Provider } from "react-redux";
+import { render } from "@testing-library/react-native";
 
+import { appReducer } from "../src/redux/reducer";
 import { SongCard } from "../src/SongCard";
 
+const store = createStore(appReducer);
+
 it("renders correctly", () => {
-  renderer.create(<SongCard title="Song 123" />);
+  render(
+    <Provider store={store}>
+      <SongCard
+        id={1}
+        title="Oceansound"
+        cover="https://raw.githubusercontent.com/Learnfield-GmbH/CodingChallange/master/shared/simple%20audio%20player/data/Oceansound.png"
+        rating={3}
+        isFavorite={false}
+      />
+    </Provider>
+  );
 });
